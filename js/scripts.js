c64787fb4101385bcb1a71714b5ca2df94622e84
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-23 15:06:06 +0300

    evaluating button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-function tracker() {
-  var triangletracker[];
+function triangletracker() {
+  var triangle[];
   var triangle =.push(parseINt(prompt("key in the first side of the triangle", )));
   if (triangle[1] === triangle[2] && triangle[2] === triangle[3]){
     alert("it is an equileteral triangle");
   }
-  } else {
-    txt = "Hello " + person + "! How are you today?";
-  }
-  document.getElementById("demo").innerHTML = txt;
+  document.getElementById("button").addEventListener('click', triangleTracker);
 }
