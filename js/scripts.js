5f607024986c481498d02a70aabb0944ba874720
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-23 15:12:38 +0300

    confing the button

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function triangletracker() {
-  var triangle[];
-  var triangle =.push(parseINt(prompt("key in the first side of the triangle", )));
+function triangletracker(){
+var triangle = [];
+triangle.push(parseInt(prompt("key in the first side of the triangle ")));
   if (triangle[1] === triangle[2] && triangle[2] === triangle[3]){
     alert("it is an equileteral triangle");
   }
-  document.getElementById("button").addEventListener('click', triangleTracker);
 }
+document.getElementById("button").addEventListener('hover', triangleTracker );
