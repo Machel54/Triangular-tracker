8e2e6513f818da312ed8abd00a1d676b6c9f2165
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-23 14:40:39 +0300

    placing the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 function tracker() {
-  var txt;
-  var person = prompt("Please enter your name:", "Harry Potter");
-  if (person == null || person == "") {
-    txt = "User cancelled the prompt.";
+  var triangletracker[];
+  var triangle =.push(parseINt(prompt("key in the first side of the triangle", )));
+  if (triangle[1] === triangle[2] && triangle[2] === triangle[3]){
+    alert("it is an equileteral triangle");
+  }
   } else {
     txt = "Hello " + person + "! How are you today?";
   }
