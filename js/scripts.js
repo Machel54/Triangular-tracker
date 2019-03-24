865da9e8c7657f500596024f44c8bb406be29f41
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-24 22:58:13 +0300

    the button is stressful

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function triangleTracker(){
 	var triangle = [];
-triangle.push(parseInt(prompt("Enter the first dimension of the triangle: ")));
-		if(triangle[0]) === true || triangle[0] <= 0){
+triangle.push(parseInt(prompt("Enter the first side of the triangle: ")));
+		if(isNaN(triangle[0]) === true || triangle[0] <= 0){
 				alert("Please enter a valid dimension");
 }else{
-  triangle.push(parseInt(prompt("Enter the second dimension of the triangle:")));
-		if(triangle[1]) === true || triangle[1] <= 0){
+  triangle.push(parseInt(prompt("Enter the second side of the triangle:")));
+		if(isNaN(triangle[1]) === true || triangle[1] <= 0){
 			alert("Please enter a valid dimension");
 }else{
-	triangle.push(parseInt(prompt("Enter the third dimension of the triangle:")));
-		if(triangle[2]) === true || triangle[2] <= 0){
+	triangle.push(parseInt(prompt("Enter the third side of the triangle:")));
+		if(isNaN)(triangle[2]) === true || triangle[2] <= 0){
 			alert("Please enter a valid dimension");
 }else{
 		if((triangle[0] + triangle[1]) <= triangle[2] || (triangle[1] + triangle[2]) <= triangle[0] || (triangle[0] + triangle[2]) <= triangle[1]){
