e4287f9544b163b4820ecb6547ed62adf5e1481d
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-24 19:01:58 +0300

    button halfway there

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function myFunction() {
-  var txt;
-  if (confirm("Press a button!")) {
+  var triangle;
+  triangle = parseInt(prompt("key in first side of the triangle")) {
     txt = "You pressed OK!";
   } else {
     txt = "You pressed Cancel!";
