ff9142f6f922ab1d35b85c71a6d9728b76a37ca1
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-24 21:40:43 +0300

    the button is almost done

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  var triangle;
-  triangle = parseInt(prompt("key in first side of the triangle")) {
-    txt = "You pressed OK!";
-  } else {
-    txt = "You pressed Cancel!";
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
