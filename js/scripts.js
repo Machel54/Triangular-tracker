c9bcb572cc0f4e078136b1af3bcbd7b4109f0c29
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-24 09:45:55 +0300

    prompt button

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,6 @@ var triangle = [];
 triangle.push(parseInt(prompt("key in the first side of the triangle ")));
   if (triangle[1] === triangle[2] && triangle[2] === triangle[3]){
     alert("it is an equileteral triangle");
-  }
 }
-document.getElementById("button").addEventListener('hover', triangleTracker );
+}
+document.getElementById("button").addEventListener('click', triangletracke
