1e55b51cccdfe1732ebc9d33c8e28319204bef14
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-03-24 10:19:15 +0300

    the button is not cooperating

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-function myFunction() {
-  var txt;
-  var person = prompt("Please enter your name:", "Harry Potter");
-  if (person == null || person == "") {
-    txt = "User cancelled the prompt.";
-  } else {
-    txt = "Hello " + person + "! How are you today?";
-  }
-  document.getElementById("demo").innerHTML = txt;
+function triangletracker(){
+var triangle = [];
+triangle.push(parseInt(prompt("key in the first side of the triangle ")));
+  if (triangle[1] === triangle[2] && triangle[2] === triangle[3]) {
+    alert("it is an equileteral triangle");
+}
+document.getElementById("button").addEventListener('click', triangletracke
 }
